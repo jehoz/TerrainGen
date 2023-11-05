453a9b03bfb906b3322af2eda4d39bdfbadbffc5
holzmanj <jesseholzman96@gmail.com>
2023-11-05 10:18:05 -0500

    Add docstrings and modf decomposition to vector2

--- src/vectors.zig
+++ src/vectors.zig
@@ -4,6 +4,7 @@ pub const Vector2 = struct {
     x: f32 = 0,
     y: f32 = 0,
 
+    /// Add two vectors (self + v)
     pub fn add(self: Vector2, v: Vector2) Vector2 {
         return .{
             .x = self.x + v.x,
@@ -11,6 +12,7 @@ pub const Vector2 = struct {
         };
     }
 
+    /// Subtract two vectors (self - v)
     pub fn subtract(self: Vector2, v: Vector2) Vector2 {
         return .{
             .x = self.x - v.x,
@@ -18,6 +20,7 @@ pub const Vector2 = struct {
         };
     }
 
+    /// Scale a vector (multiply each dimention by a scalar)
     pub fn scale(self: Vector2, mag: f32) Vector2 {
         return .{
             .x = self.x * mag,
@@ -25,10 +28,13 @@ pub const Vector2 = struct {
         };
     }
 
+    /// Calculate the length of a vector
     pub fn length(self: Vector2) f32 {
         return @sqrt(self.x * self.x + self.y * self.y);
     }
 
+    /// Normalize a vector (scale to a length of 1)
+    /// Zero-vectors are unaffected.
     pub fn normalize(self: Vector2) Vector2 {
         const len = self.length();
         if (len <= 0) {
@@ -37,4 +43,30 @@ pub const Vector2 = struct {
             return self.scale(1 / len);
         }
     }
+
+    /// Split a vector into integral (cell) and fractional (offset) parts for
+    /// each of its dimentions.
+    pub fn cellOffset(self: Vector2) CellOffset2 {
+        const modf_x = std.math.modf(self.x);
+        const modf_y = std.math.modf(self.y);
+
+        return .{
+            .cell = .{
+                .x = @intFromFloat(modf_x.ipart),
+                .y = @intFromFloat(modf_y.ipart),
+            },
+            .offset = .{
+                .x = modf_x.fpart,
+                .y = modf_y.fpart,
+            },
+        };
+    }
+};
+
+pub const CellOffset2 = struct {
+    cell: struct {
+        x: i32,
+        y: i32,
+    },
+    offset: Vector2,
 };
