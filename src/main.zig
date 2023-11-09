b56f8d3ca25146b61402024b71713ffde2d47d58
holzmanj <jesseholzman96@gmail.com>
2023-11-09 12:24:20 -0500

    Animate terrain erosion over time.

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const rl = @cImport({
     @cInclude("raylib.h");
 });
+const rg = @cImport({
+    @cInclude("raygui.h");
+});
 const fnl = @cImport({
     @cInclude("FastNoiseLite.h");
 });
@@ -13,12 +16,11 @@ pub fn main() !void {
     const screenWidth: c_int = 800;
     const screenHeight: c_int = 600;
 
-    rl.InitWindow(screenWidth, screenHeight, "raylib example window");
+    rl.InitWindow(screenWidth, screenHeight, "Terrain Generator");
     defer rl.CloseWindow();
 
     rl.SetTargetFPS(60);
 
-    // Set up 3D camera
     var camera: rl.Camera = .{
         .position = .{ .x = 18, .y = 21, .z = 18 },
         .target = .{ .x = 0, .y = 0, .z = 0 },
@@ -27,52 +29,119 @@ pub fn main() !void {
         .projection = rl.CAMERA_PERSPECTIVE,
     };
 
-    // generate heightmap, load to GPU and generate mesh
-    const heightmap = try genHeightmap(128, 128);
-    defer rl.UnloadImage(heightmap);
-
-    const heightmap_tex = rl.LoadTextureFromImage(heightmap);
-    defer rl.UnloadTexture(heightmap_tex);
-
-    const mesh = rl.GenMeshHeightmap(heightmap, .{ .x = 16, .y = 4, .z = 16 });
-
-    var model = rl.LoadModelFromMesh(mesh);
-    defer rl.UnloadModel(model);
-
-    model.materials[0].maps[rl.MATERIAL_MAP_DIFFUSE].texture = heightmap_tex;
-    const map_pos: rl.Vector3 = .{ .x = -8.0, .y = 0, .z = -8.0 };
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+    var scene = try TerrainScene.init(256, 256, allocator);
+    defer scene.deinit();
 
     while (!rl.WindowShouldClose()) {
         rl.UpdateCamera(&camera, rl.CAMERA_ORBITAL);
+        scene.update();
 
         rl.BeginDrawing();
         defer rl.EndDrawing();
 
-        rl.ClearBackground(rl.RAYWHITE);
+        rl.ClearBackground(rl.BLACK);
 
         rl.BeginMode3D(camera);
-        defer rl.EndMode3D();
+        scene.render();
+        rl.EndMode3D();
 
-        rl.DrawModel(model, map_pos, 1.0, rl.RED);
-        rl.DrawGrid(20, 1.0);
+        rl.DrawText(
+            rl.TextFormat("Erosion Iterations: %d", scene.erosion_iters),
+            20,
+            20,
+            20,
+            rl.LIGHTGRAY,
+        );
     }
 }
 
-fn genHeightmap(width: usize, height: usize) !rl.Image {
-    var noise = fnl.fnlCreateState();
-    noise.noise_type = fnl.FNL_NOISE_OPENSIMPLEX2;
-    noise.fractal_type = fnl.FNL_FRACTAL_RIDGED;
+const TerrainScene = struct {
+    terrain: Terrain,
+    erosion_iters: u32 = 0,
+    max_erosion_iters: u32 = 200_000,
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var terrain = try Terrain.init(
-        width,
-        height,
-        gpa.allocator(),
-    );
-    defer terrain.deinit();
+    texture: rl.Texture,
+    mesh: rl.Mesh,
+    model: rl.Model,
+    model_pos: rl.Vector3,
 
-    terrain.fillNoise(&noise);
-    HydraulicErosion.erodeTerrain(&terrain, .{ .iterations = 150_000 });
+    allocator: std.mem.Allocator,
 
-    return terrain.renderElevation();
-}
+    pub fn init(
+        width: usize,
+        height: usize,
+        allocator: std.mem.Allocator,
+    ) !TerrainScene {
+        var noise = fnl.fnlCreateState();
+        noise.noise_type = fnl.FNL_NOISE_OPENSIMPLEX2S;
+        noise.fractal_type = fnl.FNL_FRACTAL_RIDGED;
+        noise.frequency = 0.01 / (@as(f32, @floatFromInt(width)) / 128);
+
+        var terrain = try Terrain.init(
+            width,
+            height,
+            allocator,
+        );
+        terrain.fillNoise(&noise);
+
+        const heightmap = terrain.renderElevation();
+        defer rl.UnloadImage(heightmap);
+
+        var tex = rl.LoadTextureFromImage(heightmap);
+        var mesh = rl.GenMeshHeightmap(heightmap, .{ .x = 16, .y = 4, .z = 16 });
+        var model = rl.LoadModelFromMesh(mesh);
+        model.materials[0].maps[rl.MATERIAL_MAP_DIFFUSE].texture = tex;
+
+        return .{
+            .terrain = terrain,
+            .texture = tex,
+            .mesh = mesh,
+            .model = model,
+            .model_pos = .{ .x = -8, .y = 0, .z = -8 },
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *TerrainScene) void {
+        std.debug.print("Unloading terrain\n", .{});
+        self.terrain.deinit();
+        std.debug.print("Unloading model\n", .{});
+        rl.UnloadModel(self.model);
+        std.debug.print("Unloading mesh\n", .{});
+        rl.UnloadMesh(self.mesh);
+        std.debug.print("Unloading texture\n", .{});
+        rl.UnloadTexture(self.texture);
+    }
+
+    pub fn update(self: *TerrainScene) void {
+        if (self.erosion_iters < self.max_erosion_iters) {
+            self.erosion_iters += 1000;
+        } else {
+            return;
+        }
+        HydraulicErosion.erodeTerrain(&self.terrain, .{
+            .iterations = 1000,
+            .inertia = 0.05,
+            .erosion_rate = 0.05,
+            .deposition_rate = 0.5,
+            .evaporation_rate = 0.02,
+            .sediment_capacity = 10,
+            .gravity = 10,
+        });
+
+        const heightmap = self.terrain.renderElevation();
+        defer rl.UnloadImage(heightmap);
+
+        self.texture = rl.LoadTextureFromImage(heightmap);
+        self.mesh = rl.GenMeshHeightmap(heightmap, .{ .x = 16, .y = 4, .z = 16 });
+        self.model = rl.LoadModelFromMesh(self.mesh);
+        self.model.materials[0].maps[rl.MATERIAL_MAP_DIFFUSE].texture = self.texture;
+    }
+
+    pub fn render(self: *TerrainScene) void {
+        rl.DrawModel(self.model, self.model_pos, 1.0, rl.WHITE);
+        rl.DrawGrid(20, 1);
+    }
+};
